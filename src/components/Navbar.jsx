05b6234554436b9ed5e2e4e661ed547536d0e0c9
“Sealpiorgit config --global user.email “sealpioner.spmail.com <sealpioner.sp@gmail.com>
2023-04-13 10:24:56 -0500

    cambios en el nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Narbar = () => {
     <nav className={NavbarStyle.navbar}>
       <a href="#container">Inicio</a>
       <a href="#redes">¿Quien es mario?</a>
-      <a href="#salto">Portafolio</a>
+      <a href="#salto">noticias</a>
       <a href="#contactoFotter">Contacto</a>
     </nav>
   );
