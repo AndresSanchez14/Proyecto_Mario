e1861e1b8bedb4d3e5aea19e8fb9eac11004c947
AndresSanchez14 <asanchesvera@yahoo.es>
2023-04-13 10:03:18 -0500

    acmbios navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import NavbarStyle from "./css/Navbar.module.css";
+const Narbar = () => {
+  return (
+    <nav className={NavbarStyle.navbar}>
+      <a href="#container">Inicio</a>
+      <a href="#redes">¿Quien soy?</a>
+      <a href="#salto">Portafolio</a>
+      <a href="#contactoFotter">Contacto</a>
+    </nav>
+  );
+};
+
+export default Narbar;
